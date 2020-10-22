1618af92cc8f7ecee2d6a55574f8dd94635e47e9
Денис Попович <popov.den30@gmail.com>
2020-10-22 16:16:50 +0300

    Fixed code tabulations

--- Calculations/EnergoCalculator.cs
+++ Calculations/EnergoCalculator.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 ///namespace Calculations
 
-   public class EnergoCalculator
-    {
+public class EnergoCalculator
+{
     //calculation of ОСЕрік код 215
     //monthscosts масив куда помесячно вводятся суммы употребленной среднесуточной электроэнергии по месяцам 
     //sum это цифра за год среднемесячная
     public float f(float[] monthscosts)//вводяться помісячно витрати за кожен місяць і рахується сумма
-        {
+    {
         float sum = 0;
-        foreach(float m in monthscosts) 
+        foreach (float m in monthscosts)
         {
             sum += m;
         }
         return sum;
-        }
     }
+}
 
