c48980503e5fb396095c825bc2f06dbdc56544c6
Денис Попович <popov.den30@gmail.com>
2020-10-23 20:34:14 +0300

    Added add new serires window

---
+++ Experts_Economist/AddNewSeriesWindow.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Experts_Economist
+{
+    partial class AddNewSeriesWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SeriesTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Назва серії";
+            // 
+            // SeriesTextBox
+            // 
+            this.SeriesTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SeriesTextBox.Location = new System.Drawing.Point(12, 29);
+            this.SeriesTextBox.Name = "SeriesTextBox";
+            this.SeriesTextBox.Size = new System.Drawing.Size(167, 20);
+            this.SeriesTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Опис";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.AcceptsReturn = true;
+            this.DescriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DescriptionTextBox.Location = new System.Drawing.Point(12, 76);
+            this.DescriptionTextBox.Multiline = true;
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(167, 146);
+            this.DescriptionTextBox.TabIndex = 3;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.CancelButton.Location = new System.Drawing.Point(0, 260);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(196, 23);
+            this.CancelButton.TabIndex = 4;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // OKButton
+            // 
+            this.OKButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OKButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.OKButton.Location = new System.Drawing.Point(0, 237);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(196, 23);
+            this.OKButton.TabIndex = 5;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // AddNewSeriesWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 283);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SeriesTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "AddNewSeriesWindow";
+            this.Text = "Додавання нової серії";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SeriesTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
---
+++ Experts_Economist/AddNewSeriesWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Experts_Economist
+{
+    public partial class AddNewSeriesWindow : Form
+    {
+        public string SeriesName { get; private set; }
+        public string SeriesDescription { get; private set; }
+
+        public AddNewSeriesWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            SeriesName = SeriesTextBox.Text;
+            SeriesDescription = DescriptionTextBox.Text;
+        }
+    }
+}
