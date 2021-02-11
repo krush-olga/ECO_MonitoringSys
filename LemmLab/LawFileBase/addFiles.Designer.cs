3f56ea5fac6274e44517fb0927fcfc2242cba6e4
Денис Попович <popov.den30@gmail.com>
2021-02-11 16:49:09 +0200

    Jurist fixed

--- LemmLab/LawFileBase/addFiles.Designer.cs
+++ LemmLab/LawFileBase/addFiles.Designer.cs
@@ -28,74 +28,89 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "вибрать файл";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(215, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(176, 43);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "добавить в базу";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(709, 12);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(228, 452);
-            this.flowLayoutPanel1.TabIndex = 4;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(551, 10);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(152, 45);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Показать слова индексации";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Location = new System.Drawing.Point(12, 67);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(691, 397);
-            this.webBrowser1.TabIndex = 5;
-            // 
-            // addFiles
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(949, 476);
-            this.Controls.Add(this.webBrowser1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
-            this.Name = "addFiles";
-            this.Text = "addFiles";
-            this.ResumeLayout(false);
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.button3 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(171, 43);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "вибрать файл";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(800, 100);
+			this.panel1.TabIndex = 2;
+			// 
+			// panel2
+			// 
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel2.Location = new System.Drawing.Point(0, 422);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(800, 28);
+			this.panel2.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.Enabled = false;
+			this.button2.Location = new System.Drawing.Point(189, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(176, 43);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "добавить в базу";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.AutoScroll = true;
+			this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.flowLayoutPanel1.Controls.Add(this.button3);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 100);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 322);
+			this.flowLayoutPanel1.TabIndex = 4;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(3, 3);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(785, 33);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "Показать слова индексации";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// addFiles
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "addFiles";
+			this.Text = "addFiles";
+			this.panel1.ResumeLayout(false);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
 
 		}
 
