198a2d84aeb82d1984fa5bd232d85fda6899fb53
Денис Попович <popov.den30@gmail.com>
2021-02-11 16:12:52 +0200

    Added new services

---
+++ maps_2/Rivne/ReworkedMap/Services/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMap.Services
+{
+    interface ILogger
+    {
+        string FilePath { get; set; }
+
+        void Log(string text);
+        void Log(Exception ex);
+    }
+}
