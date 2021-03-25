220ead570a9405730fb5c41c8f58f169839e4a17
Денис Попович <popov.den30@gmail.com>
2021-03-25 13:56:45 +0200

    Added docs

--- maps_2/Rivne/Helpers/WebHelper.cs
+++ maps_2/Rivne/Helpers/WebHelper.cs
@@ -10,26 +10,39 @@ using System.Net.Http.Headers;
 
 namespace UserMap.Helpers
 {
+    /// <summary>
+    /// Инкапсулирует создание запросов по укзананному URL.
+    /// </summary>
     public static class WebHelper
     {
-        private static readonly Timer disposeTimer;
+        private static readonly Timer disposeTimer;     //Служит для высвобождения httpClient'a через указанное время.
         private static readonly string UserAgent;
         private static readonly object locker;
 
+        private static HttpClient httpClient;
 
         static WebHelper()
         {
             locker = new object();
 
             disposeTimer = new Timer(DisposeHTTPClient, null, Timeout.Infinite, Timeout.Infinite);
+
+            //Нужно для того, чтобы сервера знали, как возвращать ответ на запрос
             UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Chrome/51.0.2704.103 Safari/537.36";
 
+            //Нужно для указания текущего протокола безопасности для возможности создания запроса
             ServicePointManager.Expect100Continue = true;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
 
-        private static HttpClient httpClient;
-
+        /// <summary>
+        /// Возвращает тело ответа по запросу на указанный URL с указанными параметрами.
+        /// </summary>
+        /// <param name="url">URL по которому будет делается запрос.</param>
+        /// <param name="keyValues">Параметры запроса в виде &lt;название параметра, значение параметра&gt;</param>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="ArgumentNullException">Возникает, когд</exception>
+        /// <returns>Тело ответа в виде строки.</returns>
         public static async Task<string> GetFromURL(string url, Dictionary<string, string> keyValues)
         {
             if (string.IsNullOrEmpty(url))
@@ -42,7 +55,7 @@ namespace UserMap.Helpers
                 httpClient = new HttpClient();
                 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
-                disposeTimer.Change(60000, Timeout.Infinite);
+                disposeTimer.Change(300000, Timeout.Infinite);
             }
 
             var querry = CreateQuerry(url, keyValues);
