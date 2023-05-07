f71222236db5c96f8d36aec6a1d9c81421e7bd2f
DmitriyPavlichenko <pavlichenko.dmitriy@gmail.com>
2023-05-07 20:49:10 +0300

    add updated_on field to the `documents` table

--- EcoWebSystem-master/db-config/create_documents.sql
+++ EcoWebSystem-master/db-config/create_documents.sql
@@ -1,7 +1,8 @@
 CREATE TABLE documents
 (
-    `id`         VARCHAR(20) PRIMARY KEY NOT NULL,
-    `name`       TEXT UNIQUE             NOT NULL,
-    `body`       TEXT                    NOT NULL,
-    `created_on` DATE                    NOT NULL
+    `id`         VARCHAR(20) PRIMARY KEY             NOT NULL,
+    `name`       TEXT UNIQUE                         NOT NULL,
+    `body`       TEXT                                NOT NULL,
+    `created_on` DATE                                NOT NULL,
+    `updated_on` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
 )
